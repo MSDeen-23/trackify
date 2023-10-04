1292d1564a613110709d45937b3efe7a6fc03b90
sharuk <sharuk@beezlabs.com>
2023-10-04 22:41:09 +0530

    Added Cloudgateway

---
+++ backend/CloudGateway/src/test/java/com/beworkerbee/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beworkerbee.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/trackify-ui/src/scenes/Login/index.jsx
@@ -0,0 +1,120 @@
+import { Box, TextField, Button, useTheme, Typography } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { tokens } from "../../theme";
+import { alpha } from "@mui/material/styles";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import KeyOutlined from "@mui/icons-material/KeyOutlined";
+const Login = (props) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const initialValuesForLogin = {
+    email: "",
+    password: "",
+  };
+  const userSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Email is not valid")
+      .required("Email is required"),
+    password: yup.string().required("Password is required"),
+  });
+
+  const handleLogin = (values) => {
+    props.handleLogin();
+  };
+
+  return (
+    <Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Formik
+          onSubmit={handleLogin}
+          initialValues={initialValuesForLogin}
+          validationSchema={userSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box
+                backgroundColor={alpha(colors.grey[700], 0.3)}
+                width="500px"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                borderRadius="4px"
+                sx={{ boxShadow: 4 }}
+              >
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  width="300px"
+                  margin="25px 0"
+                  gap="30px"
+                >
+                  <Typography variant="h2">Welcome back!</Typography>
+                  {/* Email */}
+                  <TextField
+                    variant="outlined"
+                    type="text"
+                    label="Email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircleOutlinedIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                    name="email"
+                    error={!!touched.email && !!errors.email}
+                    helperText={touched.email && errors.email}
+                  ></TextField>
+                  {/* Password */}
+                  <TextField
+                    variant="outlined"
+                    type="password"
+                    label="Password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                    name="password"
+                    error={!!touched.password && !!errors.password}
+                    helperText={touched.password && errors.password}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <KeyOutlined />
+                        </InputAdornment>
+                      ),
+                    }}
+                  ></TextField>
+                  <Box display="flex" justifyContent="end" mt="20px">
+                    <Button type="submit" color="secondary" variant="contained">
+                      Login
+                    </Button>
+                  </Box>
+                </Box>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
