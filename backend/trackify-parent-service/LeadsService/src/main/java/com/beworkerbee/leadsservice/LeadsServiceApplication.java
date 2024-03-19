f8cd22675236e1ae1a594c82a66accd51a469f90
sharuk <sharuk@beezlabs.com>
2024-03-19 22:08:01 +0530

    Adding leads service

---
+++ backend/trackify-parent-service/LeadsService/src/main/java/com/beworkerbee/leadsservice/LeadsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beworkerbee.leadsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsServiceApplication.class, args);
+	}
+
+}
---
+++ backend/trackify-parent-service/LeadsService/src/test/java/com/beworkerbee/leadsservice/LeadsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beworkerbee.leadsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
